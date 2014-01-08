294ee1da38b93d025a036babaf469faef2aabc39
Walter Dal Mut <walter.dalmut@gmail.com>
2014-01-08 11:09:30 +0100

    Base project structure

---
+++ examples/ranging.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "ultrasonic.h"
+
+int main(void)
+{
+    printf("%lf\n", ranging());
+
+    return EXIT_SUCCESS;
+}
---
+++ src/ultrasonic.h
@@ -0,0 +1,7 @@
+#ifndef __ULTRASONIC_H__
+#define __ULTRASONIC_H__
+
+void ultrasonic_init(void);
+double ultrasonic_ranging(void);
+
+#endif
